afe287d104d1b27ff54a0fc28733b4e5e075b0e9
Shukria <shukriasul5@gmail.com>
2025-08-10 10:53:20 +0430

    Added a pie chart to compare the paid and unpaid invoices.

--- js/main.js
+++ js/main.js
@@ -19,3 +19,57 @@ const showTotals = () => {
 window.addEventListener("load", () => {
   showTotals();
 });
+
+
+let totalPaidAmount = 0;
+let totalUnpaidAmount = 0;
+
+// Calculate total amounts for paid and unpaid invoices
+invoices.forEach(invoice => {
+    if (invoice.paid) {
+        totalPaidAmount += invoice.amount; 
+    } else {
+        totalUnpaidAmount += invoice.amount; 
+    }
+});
+
+// Create the pie chart
+const chartContainer = document.getElementById('invoiceStatus').getContext('2d');
+const invoiceStatus = new Chart(chartContainer, {
+    type: 'pie',
+    data: {
+        labels: ['Paid Invoices', 'Unpaid Invoices'],
+        datasets: [{
+            label: 'Invoices',
+            data: [totalPaidAmount, totalUnpaidAmount],
+            backgroundColor: ['#FFCF50', '#FF6F61'],
+            hoverBackgroundColor: ['#e6b948', '#ff5b47']
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                position: 'top',
+                labels: {
+                    font: {
+                        size: 16, 
+                    }
+                }
+            },
+            title: {
+                display: true,
+                text: 'Invoices Overview',
+                font: {
+                    size: 20, 
+                }
+            },
+            tooltip: {
+                bodyFont: {
+                    size: 14 // Adjust the font size for tooltip text
+                }
+            }
+        }
+    }
+});
